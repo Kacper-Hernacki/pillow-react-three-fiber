0eb7b9ff729f25cce21c2172670701b442937ac6
Kacper <hernackikacper@gmail.com>
2021-02-09 19:11:17 +0100

    Updates

---
+++ precache-manifest.82f3fece2f9b52e6d41e1c374a306828.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db9a75677e7cf8d22262ce2b986376be",
+    "url": "/pillow-react-three-fiber/index.html"
+  },
+  {
+    "revision": "a7ac76728b1926a8a3fe",
+    "url": "/pillow-react-three-fiber/static/css/main.8c3df7e3.chunk.css"
+  },
+  {
+    "revision": "37a2b8f09b9153de1593",
+    "url": "/pillow-react-three-fiber/static/js/2.d8a6850d.chunk.js"
+  },
+  {
+    "revision": "bd16ffa8a645b42adcb7fc65d4c790f8",
+    "url": "/pillow-react-three-fiber/static/js/2.d8a6850d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7ac76728b1926a8a3fe",
+    "url": "/pillow-react-three-fiber/static/js/main.d7357165.chunk.js"
+  },
+  {
+    "revision": "de579766b473c3610d41",
+    "url": "/pillow-react-three-fiber/static/js/runtime-main.a35176b7.js"
+  }
+]);
